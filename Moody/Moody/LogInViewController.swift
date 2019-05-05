ca27d62ca9f4be7ae90a6b486d9d7ae2b042a0b5
Dean <deancs1997@gmail.com>
2019-05-05 11:32:10 -0400

    Renamed ViewController.swift to RegisterViewController.swift. Starting to work on the register page

--- Moody/Moody/LogInViewController.swift
+++ Moody/Moody/LogInViewController.swift
@@ -9,22 +9,57 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    @IBAction func logInButton(_ sender: Any) {
+        
+        let userName = userNameTF.text
+        let userPassword = passwordTF.text
+        
+        if userName != "Admin"
+        {
+            //Alert if the username is incorrect
+            let alertWrongUsername = UIAlertController (title: "Wrong Username", message: "The username enterred is wrong, please try again.", preferredStyle: UIAlertController.Style.alert)
+            alertWrongUsername.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alertWrongUsername,animated: true,completion: nil)
+        }
+            
+        else if userPassword != "Admin"{
+            
+            //Alert if the password is incorrect
+            let alertWrongPassword = UIAlertController(title: "Wrong Password", message: "The password entered is wrong, please try again", preferredStyle: UIAlertController.Style.alert)
+            alertWrongPassword.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alertWrongPassword, animated: true)
+        }
+        
+        else{
+           
+        }
     }
-    */
-
+    
+    @IBAction func registerButton(_ sender: Any) {
+        performSegue(withIdentifier: "registerSegue", sender: nil)
+    }
+    
 }
+
